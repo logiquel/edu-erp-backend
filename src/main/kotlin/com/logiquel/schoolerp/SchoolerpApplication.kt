798ce8265af98b7e3fb6d13bd3b7d34ce3fc7215
Anshuman Rana <anshumanrana1404@gmail.com>
2026-04-18 11:07:40 +0530

    first commit

---
+++ src/main/kotlin/com/logiquel/schoolerp/SchoolerpApplication.kt
@@ -0,0 +1,11 @@
+package com.logiquel.schoolerp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SchoolerpApplication
+
+fun main(args: Array<String>) {
+	runApplication<SchoolerpApplication>(*args)
+}
---
+++ src/test/kotlin/com/logiquel/schoolerp/SchoolerpApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.logiquel.schoolerp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SchoolerpApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

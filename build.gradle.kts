4383ccf36cd03803cb498bec31f0787dce5a1ab8
Anshuman Rana <anshumanrana1404@gmail.com>
2026-04-19 09:37:13 +0530

    feat: swagger added and working

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.8")
 
 }
 
